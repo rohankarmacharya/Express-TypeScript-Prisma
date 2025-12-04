81f376db668b35b2ac1cad3dc1848e33f99016c9
rohankarmacharya <rohankarmacharya.biz@gmail.com>
2025-12-04 13:04:42 +0545

    Validate name, age, email and password field

---
+++ Validation/src/prisma.ts
@@ -0,0 +1,15 @@
+// Creates a single PrismaClient instance to use across the entire project
+
+
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
---
+++ Validation/src/routes/userRoutes.ts
@@ -0,0 +1,126 @@
+import { Router } from "express";
+import { prisma } from "../prisma.js";
+import bcrypt from 'bcrypt';
+
+const router = Router();
+
+//=========Create User==========//
+router.post("/", async (req, res) => {
+  const { name, age, email, password } = req.body;
+
+  try {
+    /////==========VALIDATIONS=========//
+    if (!name || !age || !email || !password) {
+      return res.status(400).json({ error: "All fields are required." });
+    }
+
+    if(age <= 18){
+        return res.status(400).json({
+            error: "Age must be greater than 18."
+        });
+    }
+
+    if(!email.endsWith("@gmail.com")){
+        return res.status(400).json({
+            error: "Email must end with @gmail.com"
+        });
+    };
+
+    const existingUser = await prisma.user.findUnique({
+        where: {email}
+    });
+
+    if(existingUser){
+       return res.status(400).json({
+             error: "Email already exists. Please use another email."
+        });
+    }
+
+    const startsWithCapital = /^[A-Z]/.test(password);
+    if(!startsWithCapital){
+        return res.status(400).json({
+            error: "Password must start with a capital letter."
+        });
+    }
+
+    const hasSpecialChar = /[!@#$%^&*(),.?;:'"{}|<>]/.test(password);
+
+    if(!hasSpecialChar){
+        return res.status(400).json({
+            error: "Password must contain at least one special character."
+        });
+    }
+
+//=============HASH PASSWORD USING BCRYPT===============///////
+const hashedPassword = await bcrypt.hash(password, 10);   //10 - salt around: number of iterations bcrypt uses when generating password
+
+//==========CREATE A USER================//
+    const user = await prisma.user.create({
+      data: { name, age, email, password: hashedPassword },
+    });
+    res.json({ message: "User Created!", user });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//===========GET ALL USERS============//
+router.get("/", async (_req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//==========GET USER BY ID===============//
+router.get("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    res.json(user);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//=================UPDATE USER==================//
+router.put("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const { name, age, email, password } = req.body;
+
+  try {
+
+    let hashedPassword = undefined;
+
+    if(password){
+        hashedPassword = await bcrypt.hash(password, 10);
+    }
+    const user = await prisma.user.update({
+      where: { id },
+      data: { name, age, email, password: hashedPassword},
+    });
+    res.json({ message: "User updated!", user });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//=================DELETE USER==============//
+router.delete("/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    await prisma.user.delete({ where: { id } });
+    res.json({ message: "User deleted!" });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
